eca2b46aecff09222de41d914e935887f4d60d58
Surya <surya.aimlx@gmail.com>
2025-07-16 23:28:56 +0530

    [Schema]:Added User & Post Models with Init Migrations

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/api/v1/signup' , (req,res)=>
+{
+
+});
+
+app.post('/api/v1/signin' , (req,res)=>
+{
+
+});
+
+app.post('/api/v1/blog' , (req,res)=> 
+{
+
+});
+
+app.put('/api/v1/blog' , (req,res)=>
+{
+
+});
+
+app.get('/api/v1/blog/:id' , (req,res)=>
+{
+
+});
+
+export default app
