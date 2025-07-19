77df3da5837a0d4e784edc4a03aa51daba2c205d
Surya <surya.aimlx@gmail.com>
2025-07-19 21:34:37 +0530

    Frontend Files

---
+++ frontend/frontend/src/components/Quote.tsx
@@ -0,0 +1,12 @@
+export const Quote = ()=> 
+{
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+        <div className="max-w-md text-center text-3xl font-bold">
+            
+            Hi from the Quote Component
+        </div>
+        </div>
+    
+    </div>
+}
---
+++ frontend/frontend/src/pages/Signup.tsx
@@ -0,0 +1,18 @@
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
+
+export const Signup = () =>
+    {
+        return <div>
+
+            <div>
+                <Auth/>
+            </div>
+
+            <div>
+                <Quote/>
+            </div>
+            
+        </div>
+
+    }
